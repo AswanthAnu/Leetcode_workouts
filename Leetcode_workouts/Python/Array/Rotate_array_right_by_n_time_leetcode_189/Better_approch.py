41de3bdfaa04ccd13eb4d3d56d270116ae9433ad
AswanthAnu <aswanthanu777@gmail.com>
2025-11-01 22:59:10 +0000

    right rotate array by n times

---
+++ Leetcode_workouts/Python/Array/Rotate_array_right_by_n_time_leetcode_189/Better_approch.py
@@ -0,0 +1,17 @@
+class solution:
+    def rotate(self, nums, d):
+        n = len(nums)
+        d = d % n
+        new = []
+
+        for i in range(0, d):
+            new.append(nums[n - d + i])
+
+        for j in range(1, n - d + 1):
+            nums[n - j] = nums[n - d - j]
+        
+        for k in range(0, d):
+            nums[k] = new[k]
+
+        return nums
+            
